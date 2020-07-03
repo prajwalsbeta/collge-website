ff9a9162647027041b649f5ffa01cd3ea3ef4ece
kmangesh49 <kumarmangesh55@gmail.com>
2020-07-03 13:49:37 +0530

    v1🎉

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 const firebaseConfig = {
 	apiKey: 'AIzaSyDhz2KWMu0QUKziPbO5Aql4qbljwU09aac',
 	authDomain: 'hackathon-e1052.firebaseapp.com',
@@ -15,4 +16,5 @@ firebase.initializeApp(firebaseConfig)
 
 export const firestore = firebase.firestore()
 export const auth = firebase.auth()
+export const storage = firebase.storage()
 export default firebase
--- js/register.js
+++ js/register.js
@@ -14,6 +14,7 @@ export function createTeam(info) {
 		.then(() => {
 			form.reset()
 			thanks.removeAttribute('hidden')
+			loader.setAttribute('hidden', true)
 		})
 		.catch((err) => {
 			error.removeAttribute('hidden', '')
@@ -31,7 +32,9 @@ async function submit(event) {
 	let teamLeaderName = document.querySelector("input[name='team-leader-name']")
 		.value
 	let mobileNo = document.querySelector("input[name='mobile-no']").value
-	let projectTheme = document.querySelector("input[name='project-theme']").value
+	let state = document.querySelector("input[name='state']").value
+	let city = document.querySelector("input[name='city']").value
+	let pincode = document.querySelector("input[name='pincode']").value
 	let email = document.querySelector("input[name='email']").value
 	let password = document.querySelector("input[name='password']").value
 
@@ -39,8 +42,10 @@ async function submit(event) {
 		teamName: teamName,
 		teamLeaderName: teamLeaderName,
 		mobileNo: mobileNo,
-		projectTheme: projectTheme,
 		email: email,
+		state,
+		city,
+		pincode,
 		password: password,
 	}
 	await createTeam(info)
